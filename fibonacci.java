438d53ae0b1204f799d1398dd9cc29bcdfbdb449
0x1db6418de <59483698+0x1db6418de@users.noreply.github.com>
2020-01-04 16:49:22 +0100

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,20 @@
+static void fibonacci(int x){
+        int a = 1; 
+        int b = 1;
+        int c = a+b;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i = 0; i < x; i++){
+            System.out.println(c);
+            b = c;
+            c = a + c;
+            a = b;
+        }
+    }
+    
+    public static void main(String[] args){
+       
+        
+        fibonacci(10);
+        
+    }  
