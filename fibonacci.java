c08a9e328a86ad4e390e93ee136e3d3bb56dbe11
0x1db6418de <59483698+0x1db6418de@users.noreply.github.com>
2020-01-05 20:11:20 +0100

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,31 +1,34 @@
-static void fibonacci(int x){
-    int a = 1; 
-    int b = 1;
-    int c = a+b;
-    System.out.println(a);
-    System.out.println(b);
-    for(int i = 0; i < x; i++){
-        System.out.println(c);
-        b = c;
-        c = a + c;
-        a = b;
+public class Fibonacci {
+
+    static void fibonacci(int x){
+        int a = 1; 
+        int b = 1;
+        int c = a+b;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i = 0; i < x; i++){
+            System.out.println(c);
+            b = c;
+            c = a + c;
+            a = b;
+        }
     }
+
+    public static void main(String[] args){  
+
+        fibonacci(10);
+
+        /*
+        // Optional solution with arrays
+        int fib [] = new int[10];
+        fib[0] = 1;
+        fib[1] = 1;
+        for(int i = 0; i < 8; i++){
+            fib[i+2] = fib[i+1] + fib[i];
+        }
+        for(int i = 0; i < 10; i++){
+            System.out.println(fib[i]);   
+        }
+        */
+    }  
 }
-    
-public static void main(String[] args){  
-        
-    fibonacci(10);
-    
-    /*
-    // Optional solution with arrays
-    int fib [] = new int[10];
-    fib[0] = 1;
-    fib[1] = 1;
-    for(int i = 0; i < 8; i++){
-        fib[i+2] = fib[i+1] + fib[i];
-    }
-    for(int i = 0; i < 10; i++){
-        System.out.println(fib[i]);   
-    }
-    */
-}  
