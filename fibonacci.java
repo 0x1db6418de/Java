194478c6944218ad182475a471e252e53fd9695e
0x1db6418de <59483698+0x1db6418de@users.noreply.github.com>
2020-01-05 16:38:50 +0100

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -15,5 +15,17 @@ static void fibonacci(int x){
 public static void main(String[] args){  
         
     fibonacci(10);
-        
+    
+    /*
+    // Optional solution with arrays
+    int fib [] = new int[10];
+    fib[0] = 1;
+    fib[1] = 1;
+    for(int i = 0; i < 8; i++){
+        fib[i+2] = fib[i+1] + fib[i];
+    }
+    for(int i = 0; i < 10; i++){
+        System.out.println(fib[i]);   
+    }
+    */
 }  
